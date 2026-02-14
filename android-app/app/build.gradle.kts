6993182c76b8e7e7ece0677c5e1e6dcca4994790
pbhaskal <premkumar_bhaskal@intuit.com>
2026-02-14 22:51:41 +0530

    version v0.3

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.prembhaskal.expensetracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         ndk {
             debugSymbolLevel = "SYMBOL_TABLE"
